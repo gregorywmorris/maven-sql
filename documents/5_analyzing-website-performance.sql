627bd0bce0a3e14bead0efc45963e16a566e58f5
gregorywmorris <gwm1980@gmail.com>
2024-08-23 10:11:14 -0400

    currently on section 5

--- documents/5_analyzing-website-performance.sql
+++ documents/5_analyzing-website-performance.sql
@@ -74,4 +74,126 @@ WHERE
 GROUP BY
   landing_page;
 
---
+-- Identify landing page bounce rate - overbuilt??
+DROP TABLE IF EXISTS first_pageview;
+CREATE TEMPORARY TABLE first_pageview AS
+SELECT
+  website_pageviews.website_session_id,
+  MIN(website_pageviews.website_pageview_id) AS min_pv_id
+FROM
+  website_pageviews
+  INNER JOIN website_sessions
+    ON website_sessions.website_session_id = website_pageviews.website_session_id
+    AND website_sessions.created_at BETWEEN '2014-01-01' AND '2014-02-01'
+GROUP BY
+  website_pageviews.website_session_id;
+
+DROP TABLE IF EXISTS sessions_w_landing_page
+CREATE TEMPORARY TABLE sessions_w_landing_page AS
+SELECT
+  first_pageview.website_session_id,
+  website_pageviews.pageview_url AS landing_page
+FROM
+  first_pageview
+LEFT JOIN
+  website_pageviews
+ON
+  website_pageviews.website_pageview_id = first_pageview.min_pv_id;
+
+DROP TABLE IF EXISTS bounced_sessions_only
+CREATE TEMPORARY TABLE bounced_sessions_only AS
+SELECT
+  sessions_w_landing_page.website_session_id,
+  sessions_w_landing_page.landing_page,
+  COUNT(website_pageviews.website_pageview_id) AS count_of_pageviews
+FROM
+  sessions_w_landing_page
+LEFT JOIN
+  website_pageviews ON website_pageviews.website_session_id = sessions_w_landing_page.website_session_id
+GROUP BY
+  sessions_w_landing_page.website_session_id,
+  sessions_w_landing_page.landing_page
+HAVING
+  count(website_pageviews.website_pageview_id) = 1;
+
+SELECT
+  sessions_w_landing_page.landing_page,
+  COUNT(DISTINCT sessions_w_landing_page.website_session_id) AS sessions,
+  COUNT(DISTINCT bounced_sessions_only.website_session_id) AS bounced_sessions,
+  COUNT(DISTINCT bounced_sessions_only.website_session_id) / COUNT(DISTINCT sessions_w_landing_page.website_session_id) AS bounce_rate
+FROM
+  sessions_w_landing_page
+LEFT JOIN bounced_sessions_only
+  ON sessions_w_landing_page.website_session_id =  bounced_sessions_only.website_session_id
+GROUP BY
+  sessions_w_landing_page.landing_page;
+
+-- homepage bounce rate
+DROP TABLE IF EXISTS first_pageview;
+CREATE TEMPORARY TABLE first_pageview AS
+SELECT
+  website_pageviews.website_session_id,
+  MIN(website_pageviews.website_pageview_id) AS min_pv_id
+FROM
+  website_pageviews
+WHERE
+  website_pageviews.created_at < '2012-06-14'
+GROUP BY
+  website_pageviews.website_session_id;
+
+DROP TABLE IF EXISTS sessions_w_landing_page
+CREATE TEMPORARY TABLE sessions_w_landing_page AS
+SELECT
+  first_pageview.website_session_id,
+  website_pageviews.pageview_url AS landing_page
+FROM
+  first_pageview
+LEFT JOIN
+  website_pageviews
+ON
+  website_pageviews.website_pageview_id = first_pageview.min_pv_id;
+
+DROP TABLE IF EXISTS bounced_sessions_only
+CREATE TEMPORARY TABLE bounced_sessions_only AS
+SELECT
+  sessions_w_landing_page.website_session_id,
+  sessions_w_landing_page.landing_page,
+  COUNT(website_pageviews.website_pageview_id) AS count_of_pageviews
+FROM
+  sessions_w_landing_page
+LEFT JOIN
+  website_pageviews
+  ON website_pageviews.website_session_id = sessions_w_landing_page.website_session_id
+GROUP BY
+  sessions_w_landing_page.website_session_id,
+  sessions_w_landing_page.landing_page
+HAVING
+  count(website_pageviews.website_pageview_id) = 1;
+-- Calculate bounce rate 1
+SELECT
+  COUNT(DISTINCT sessions_w_landing_page.website_session_id) AS sessions,
+  COUNT(DISTINCT bounced_sessions_only.website_session_id) AS bounced_sessions,
+  COUNT(DISTINCT bounced_sessions_only.website_session_id) / COUNT(DISTINCT sessions_w_landing_page.website_session_id) AS bounce_rate
+FROM
+  sessions_w_landing_page
+LEFT JOIN bounced_sessions_only
+  ON sessions_w_landing_page.website_session_id =  bounced_sessions_only.website_session_id;
+--Alternative
+-- Create a temporary table to store total sessions and bounced sessions
+CREATE TEMPORARY TABLE total_bounced_sessions AS
+SELECT
+  COUNT(DISTINCT sessions_w_landing_page.website_session_id) AS total_sessions,
+  COUNT(DISTINCT CASE WHEN bounced_sessions_only.website_session_id IS NOT NULL THEN sessions_w_landing_page.website_session_id END) AS bounced_sessions
+FROM
+  sessions_w_landing_page
+LEFT JOIN bounced_sessions_only
+  ON sessions_w_landing_page.website_session_id =  bounced_sessions_only.website_session_id;
+-- Calculate bounce rate
+SELECT
+  total_sessions,
+  bounced_sessions,
+  bounced_sessions / total_sessions AS bounce_rate
+FROM
+  total_bounced_sessions;
+
+-- Paid search landing page comparison
